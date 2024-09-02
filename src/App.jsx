b1211a4860144bfa151dda118cd361cf6a8cdb8b
icecam0001 <kupermancameron@gmail.com>
2024-09-02 18:23:28 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+import { useState } from 'react'
+
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>{props.text}</button>
+  )
+}
+
+const Anecdote = ({anec, votes}) => {
+  return(
+    <div>
+      <p>{anec}</p>
+      <p>has {votes} votes</p>
+    </div>
+  )
+}
+
+const Winner = ({anecdotes, allVotes}) => {
+  const highestVoteCount = Math.max(...allVotes)
+  const winnerIndex = allVotes.indexOf(highestVoteCount)
+  const winner = anecdotes[winnerIndex]
+  if (highestVoteCount === 0) {
+    return (
+      
+      <p>No votes yet</p>
+    )
+  }
+  return (
+    <div>
+      <h1>Anecdote with the most votes</h1>
+      <p>{winner}</p>
+      <p>has {highestVoteCount} votes</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+    
+  const [selected, setSelected] = useState(0)
+  const [allVotes, setAllVotes] = useState(Array(anecdotes.length).fill(0))
+
+  const nextClick = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length)
+    setSelected(randomIndex)
+  }
+
+  const handleVoteClick = () => {
+    const newAllVotes = [...allVotes]
+    newAllVotes[selected] += 1
+    setAllVotes(newAllVotes)
+  }
+
+  return (
+    <div>
+      <h1>Software Engineering Anecdote</h1>
+      <Anecdote anec={anecdotes[selected]} votes={allVotes[selected]} />
+      <Button handleClick={nextClick} text='Next anecdote'/>
+      <Button handleClick={handleVoteClick} text='Vote'/>
+      <Winner anecdotes={anecdotes} allVotes={allVotes} />
+    </div>
+  )
+}
+
+export default App
